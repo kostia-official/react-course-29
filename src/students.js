0e0d7b97039118d925203cad9161d670ff1f0900
Konstantin Zv <kozzztya@gmail.com>
2020-06-12 13:19:04 +0300

    Add small CSS fixes Disable answer button click if no present students Move data files to the separate folder

--- src/students.js
+++
@@ -1,8 +0,0 @@
-import faker from 'faker';
-
-export const getStudents = () => [
-  { id: faker.random.uuid(), name: 'Александр Друзь', score: 0, isPresent: true },
-  { id: faker.random.uuid(), name: 'Борис Бурда', score: 0, isPresent: true },
-  { id: faker.random.uuid(), name: 'Джон Реактов', score: 0, isPresent: true },
-  { id: faker.random.uuid(), name: 'Майкл Стаб', score: 0, isPresent: true },
-];
